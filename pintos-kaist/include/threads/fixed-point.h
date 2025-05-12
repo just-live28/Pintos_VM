78aa5a97d76324ac87ff2445a85c3df15ac0086d
Junhyuk <jhim0228@gmail.com>
2025-05-12 20:06:23 +0900

    fixed-point.h 수정

--- pintos-kaist/include/threads/fixed-point.h
+++ pintos-kaist/include/threads/fixed-point.h
@@ -1,39 +1,28 @@
 #ifndef THREADS_FIXED_POINT_H
 #define THREADS_FIXED_POINT_H
 
+#include <stdint.h>
+
 typedef int fixed_t;
 
 #define SHIFT 14
 #define F (1<<SHIFT)
 
-// 변환 함수
-fixed_t int_to_fixed(int n);
-int fixed_to_int(fixed_t x);
-int fixed_to_int_round(fixed_t x);
-
-// 연산 함수
-fixed_t add_fixed(fixed_t x, fixed_t y);
-fixed_t sub_fixed(fixed_t x, fixed_t y);
-fixed_t mul_fixed(fixed_t x, fixed_t y);
-fixed_t div_fixed(fixed_t x, fixed_t y);
-
-fixed_t fixed_mul_int(fixed_t x, int n);
-fixed_t fixed_div_int(fixed_t x, int n);
 
 
 
-
-fixed_t int_to_fixed(int n){
+//fixed_t 변환 함수
+static inline fixed_t int_to_fixed(int n){
     return n << SHIFT;
 }
 
 //단순 버림
-int fixed_to_int(fixed_t x){
+static inline int fixed_to_int(fixed_t x){
     return x >> SHIFT;
 }
 
 //14번째 비트값 변동으로 반올림 구현
-int fixed_to_int_round(fixed_t x){
+static inline int fixed_to_int_round(fixed_t x){
     int a;
     if(x >= 0)
         a = x + (F>>1); // x + 0.5 
@@ -43,25 +32,25 @@ int fixed_to_int_round(fixed_t x){
 }
 
 // 연산 함수
-fixed_t add_fixed(fixed_t x, fixed_t y){
+static inline fixed_t add_fixed(fixed_t x, fixed_t y){
     return x + y;
 }
 
-fixed_t sub_fixed(fixed_t x, fixed_t y){
+static inline fixed_t sub_fixed(fixed_t x, fixed_t y){
     return x - y;
 }
 
-fixed_t mul_fixed(fixed_t x, fixed_t y){
+static inline fixed_t mul_fixed(fixed_t x, fixed_t y){
     return ((int64_t)x * y) >> SHIFT;
 }
-fixed_t div_fixed(fixed_t x, fixed_t y){
+static inline fixed_t div_fixed(fixed_t x, fixed_t y){
     return ((int64_t)x << SHIFT) / y;
 }
 
-fixed_t fixed_mul_int(fixed_t x, int n){
+static inline fixed_t fixed_mul_int(fixed_t x, int n){
     return x * n;
 }
-fixed_t fixed_div_int(fixed_t x, int n){
+static inline fixed_t fixed_div_int(fixed_t x, int n){
     return x / n;
 }
 
